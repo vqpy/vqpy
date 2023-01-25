432bbe07a6f1be6dbe23dae78c50a98b8cc3c989
Shan Yu <shanyu1@g.ucla.edu>
2023-01-25 03:39:30 +0000

    Support pip install

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import setuptools
+from setuptools import setup, find_packages
 
 
 def get_version() -> str:
@@ -11,14 +11,23 @@ def get_description():
     return open("README.md", "r", encoding="utf-8").read()
 
 
-setuptools.setup(
+setup(
     name="vqpy",
     version=get_version(),
     description="A language for video analytics",
     long_description=get_description(),
     long_description_content_type="text/markdown",
     package_dir={"vqpy": "vqpy"},
-    packages=['vqpy'],
+    packages=find_packages(include=['vqpy', "vqpy.*"]),
     python_requires=">=3.7",
-    install_requires=["loguru", "tqdm", "onnxruntime", "shapely", "scipy"],
+    install_requires=["loguru",
+                      "tqdm",
+                      "onnxruntime",
+                      "shapely",
+                      "scipy",
+                      "numpy<1.24.0",
+                      "lap",
+                      "cython_bbox",
+                      "requests",
+                      ],
 )
