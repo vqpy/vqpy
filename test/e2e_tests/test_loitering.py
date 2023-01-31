a93c240da1f1914fab42da7eb563d0e9e58a9984
Hanry <54668181+hanryxu@users.noreply.github.com>
2023-01-31 21:30:49 +0000

    Use fake detector with different names for precomputed results from different videos

--- test/e2e_tests/test_loitering.py
+++ test/e2e_tests/test_loitering.py
@@ -10,11 +10,12 @@ root = Path(__file__).parent.parent.parent
 video_name = "loitering"
 video_ext = "mp4"
 task_name = "loitering"
+fake_detector_name = f"fake-yolox-{video_name}"
 video_path = (root / f"videos/{video_name}.{video_ext}").as_posix()
 precomputed_path = (root / f"precomputed/{video_name}_yolox.pkl").as_posix()
 save_folder = (root / f"e2e_outputs/").as_posix()
 result_path = (
-    root / f"e2e_outputs/{video_name}_{task_name}_fake-yolox.json"
+    root / f"e2e_outputs/{video_name}_{task_name}_{fake_detector_name}.json"
 ).as_posix()
 expected_result_path = (
     root / f"expected_results/{video_name}_{task_name}_yolox.json"
@@ -56,14 +57,14 @@ class People_loitering_query(vqpy.QueryBase):
 
 
 def test_loitering():
-    register("fake-yolox", FakeYOLOX, precomputed_path, None)
+    register(fake_detector_name, FakeYOLOX, precomputed_path, None)
     vqpy.launch(
         cls_name=COCO_CLASSES,
         cls_type={"person": Person},
         tasks=[People_loitering_query()],
         video_path=video_path,
         save_folder=save_folder,
-        detector_name="fake-yolox",
+        detector_name=fake_detector_name,
     )
 
     compare(result_path, expected_result_path)
--- test/e2e_tests/test_unattended_baggage.py
+++ test/e2e_tests/test_unattended_baggage.py
@@ -10,11 +10,12 @@ root = Path(__file__).parent.parent.parent
 video_name = "unattended_baggage"
 video_ext = "mp4"
 task_name = "unattended_baggage"
+fake_detector_name = f"fake-yolox-{video_name}"
 video_path = (root / f"videos/{video_name}.{video_ext}").as_posix()
 precomputed_path = (root / f"precomputed/{video_name}_yolox.pkl").as_posix()
 save_folder = (root / f"e2e_outputs/").as_posix()
 result_path = (
-    root / f"e2e_outputs/{video_name}_{task_name}_fake-yolox.json"
+    root / f"e2e_outputs/{video_name}_{task_name}_{fake_detector_name}.json"
 ).as_posix()
 expected_result_path = (
     root / f"expected_results/{video_name}_{task_name}_yolox.json"
@@ -90,14 +91,14 @@ class FindUnattendedBaggage(vqpy.QueryBase):
 
 
 def test_unattended_baggage():
-    register("fake-yolox", FakeYOLOX, precomputed_path, None)
+    register(fake_detector_name, FakeYOLOX, precomputed_path, None)
     vqpy.launch(
         cls_name=COCO_CLASSES,
         cls_type={"person": Person, "backpack": Baggage, "suitcase": Baggage},
         tasks=[FindUnattendedBaggage()],
         video_path=video_path,
         save_folder=save_folder,
-        detector_name="fake-yolox",
+        detector_name=fake_detector_name,
     )
 
     compare(result_path, expected_result_path)
