bab7dc4bd8e4fed20835a77539a996df6b087cf5
Shan Yu <shanyu1@g.ucla.edu>
2023-02-22 22:11:32 +0000

    addd has_next in operator base

--- vqpy/backend/operator/base.py
+++ vqpy/backend/operator/base.py
@@ -4,6 +4,16 @@ from abc import abstractmethod
 
 class Operator:
 
+    def __init__(self, prev=None) -> None:
+        self.prev = prev
+
+    def has_next(self) -> bool:
+        # video reader and stateful projector need to overwrite.
+        if self.prev:
+            return self.prev.has_next()
+        else:
+            raise NotImplementedError
+
     @abstractmethod
     def next() -> Frame:
         pass
