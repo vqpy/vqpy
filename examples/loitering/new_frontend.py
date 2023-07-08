22697524a3b6be54d293b6e930765c18e47f6a79
Shan Yu <shanyu1@g.ucla.edu>
2023-07-08 11:59:10 -0700

    Fix region and add warning in loitering (#65)
    * fix region and add warning
    
    * fix flake

--- examples/loitering/new_frontend.py
+++ examples/loitering/new_frontend.py
@@ -1,11 +1,14 @@
 import argparse
 import vqpy
 from typing import List, Tuple
-import numpy as np
 import ast
 
 from vqpy.frontend.vobj import VObjBase, vobj_property
 from vqpy.frontend.query import QueryBase
+
+NO_RISK = 0
+WARNING = 1
+ALARM = 2
 
 
 def make_parser():
@@ -21,7 +24,7 @@ def make_parser():
     parser.add_argument(
         "--polygon",
         default=(
-            "[(550, 550), (1162, 400), (1720, 720), (1430, 1072), (600, 1073)]"
+            "[(360, 367), (773, 267), (1143, 480), (951, 715), (399, 715)]"
         ),
         help="polygon to define the region of interest",
     )
@@ -48,17 +51,19 @@ class Person(VObjBase):
         return (tlbr[:2] + tlbr[2:]) / 2
 
     # TODO: continuing not yet supported, workaround
-    @vobj_property(inputs={"bottom_center": 9})
+    @vobj_property(inputs={"bottom_center": 150})
     def in_region(self, values):
         bottom_centers = values["bottom_center"]
-        in_region = np.all(
-            [
-                bottom_center is not None
-                and vqpy.query.utils.within_regions(REGIONS)(bottom_center)
-                for bottom_center in bottom_centers
-            ]
-        )
-        return in_region
+
+        def point_in_region(x):
+            return x is not None and \
+                vqpy.query.utils.within_regions(REGIONS)(x)
+        nframes_warning = 60
+        if all(list(map(point_in_region, bottom_centers))):
+            return ALARM
+        if all(list(map(point_in_region, bottom_centers[-nframes_warning:]))):
+            return WARNING
+        return NO_RISK
 
 
 class People_loitering_query(QueryBase):
@@ -66,14 +71,18 @@ class People_loitering_query(QueryBase):
         self.person = Person()
 
     def frame_constraint(self):
-        return self.person.in_region == True  # noqa: E712
+        return (self.person.in_region == ALARM) | (
+            self.person.in_region == WARNING
+        )
 
     def frame_output(self):
-        return (self.person.center, self.person.tlbr)
+        return (self.person.center, self.person.tlbr, self.person.in_region)
 
 
 if __name__ == "__main__":
     args = make_parser().parse_args()
+    # Note that the loitering.mp4 is of shape 1274*720, therefore the polygon
+    # points differs from that in the old example, with shape of 1920*1080.
     REGIONS = [ast.literal_eval(args.polygon)]
 
     query_executor = vqpy.init(
