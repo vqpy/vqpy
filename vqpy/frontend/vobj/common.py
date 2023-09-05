60613d1380fbe2f50565af5cd51065bb84f40bd5
Shan Yu <shanyu1@g.ucla.edu>
2023-09-05 23:50:57 +0000

    remove duplicates in property

--- vqpy/frontend/vobj/common.py
+++ vqpy/frontend/vobj/common.py
@@ -1,5 +1,6 @@
 def get_dep_properties(prop):
     vobj_properties = []
+    vobj_properties_names = set()
     if prop.is_vobj_property():
         all_dep_names = prop.inputs.keys()
         built_in_names = prop.vobj.get_builtin_property_names()
@@ -8,7 +9,13 @@ def get_dep_properties(prop):
             if name == prop.name:
                 continue
             p = prop.vobj.get_property(name)
-            vobj_properties.extend(get_dep_properties(p))
+            dep_properties = get_dep_properties(p)
+            # remove duplicates
+            for dep_prop in dep_properties:
+                if dep_prop.name not in vobj_properties_names:
+                    vobj_properties.append(dep_prop)
+                    vobj_properties_names.add(dep_prop.name)
         vobj_properties.append(prop)
+        vobj_properties_names.add(prop.name)
 
     return vobj_properties
